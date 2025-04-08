d062de12db09d723e02172d34466e6e8fed1a9a8
suyeon9456 <suyeon9456@naver.com>
2025-04-08 23:31:26 +0900

    feat: 플리핑 페이지 컴포넌트의 페이지 수 조정 및 스타일 수정

--- src/component/FlippingPages/index.tsx
+++ src/component/FlippingPages/index.tsx
@@ -1,13 +1,13 @@
 import { motion } from 'framer-motion';
-
+// initial={{ rotateY: 0, skewY: 0, scale: 1, x: 0 }}
+//                     animate={{ rotateY: -180, skewY: 2, scale: 1.2, x: 100 }}
 const FlippingPages = () => {
-    const pages = Array(15).fill(0);
-    const halfIndex = Math.floor(pages.length / 2);
+    const pages = Array(8).fill(0);
     return (
-        <div className="relative w-[400px] h-[500px] perspective-[800px] mx-auto">
-            {pages.slice(0, halfIndex).map((content, index) => (
+        <motion.div className="relative w-[400px] h-[500px] perspective-[800px]">
+            {pages.map((_, index) => (
                 <motion.div
-                    className="absolute top-0 left-0 w-full h-full bg-white border border-gray-300 flex items-center justify-center backface-visibility-hidden"
+                    className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden"
                     key={index}
                     style={{
                         zIndex: pages.length - index,
@@ -15,15 +15,26 @@ const FlippingPages = () => {
                         transformOrigin: 'left',
                     }}
                     initial={{ rotateY: 0, skewY: 0 }}
-                    animate={{ rotateY: -180, skewY: 2 }}
+                    animate={{ rotateY: -180, skewY: 0 }}
                     transition={{
                         duration: 0.6,
                         delay: index * 0.1,
                         ease: 'easeInOut',
                     }}
-                ></motion.div>
+                >
+                    <div className="bg-white" />
+                </motion.div>
             ))}
-        </div>
+            <div
+                className="rounded-sm bg-yellow-400 z-0 origin-center h-full flex items-center justify-center"
+                style={{
+                    backgroundImage: `linear-gradient(45deg, rgba(0,0,0,0.1) 25%, transparent 25%, transparent 50%, rgba(0,0,0,0.1) 50%, rgba(0,0,0,0.1) 75%, transparent 75%, transparent)`,
+                    backgroundSize: '5px 5px',
+                    backgroundPosition: '0 0',
+                    backgroundRepeat: 'repeat',
+                }}
+            />
+        </motion.div>
     );
 };
 
