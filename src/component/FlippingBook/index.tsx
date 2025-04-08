22652de8f06fc28ed6086d175f026327a0760670
suyeon9456 <suyeon9456@naver.com>
2025-04-08 23:52:44 +0900

    feat: 책 표지 내부 컴포넌트 추가 및 플리핑 페이지 컴포넌트에 적용

--- src/component/FlippingBook/index.tsx
+++ src/component/FlippingBook/index.tsx
@@ -1,9 +1,11 @@
 import { motion } from 'framer-motion';
+import BookCoverInner from './BookCoverInner';
 
 const FlippingBook = () => {
     const pages = Array(8).fill(0);
     return (
         <motion.div className="relative w-[400px] h-[500px] perspective-[800px]">
+            <BookCoverInner className="absolute top-0 left-[-400px]" />
             {pages.map((_, index) => (
                 <motion.div
                     className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden"
@@ -14,7 +16,7 @@ const FlippingBook = () => {
                         transformOrigin: 'left',
                     }}
                     initial={{ rotateY: 0, skewY: 0 }}
-                    animate={{ rotateY: -180, skewY: 0 }}
+                    animate={{ rotateY: -180, skewY: 0 - (pages.length - index) }}
                     transition={{
                         duration: 0.6,
                         delay: index * 0.1,
@@ -24,6 +26,14 @@ const FlippingBook = () => {
                     <div className="bg-white" />
                 </motion.div>
             ))}
+            <motion.div
+                className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden"
+                initial={{ rotateY: 0, skewY: -1 }}
+                style={{
+                    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
+                }}
+            />
+            <BookCoverInner />
         </motion.div>
     );
 };
