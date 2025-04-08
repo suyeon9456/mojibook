7809ef9aa6950230ad0e5b66f6a785057f55443c
suyeon9456 <suyeon9456@naver.com>
2025-04-08 23:42:45 +0900

    refac: 플리핑 페이지 컴포넌트 이름 변경

--- src/component/FlippingBook/index.tsx
+++ src/component/FlippingBook/index.tsx
@@ -1,7 +1,6 @@
 import { motion } from 'framer-motion';
-// initial={{ rotateY: 0, skewY: 0, scale: 1, x: 0 }}
-//                     animate={{ rotateY: -180, skewY: 2, scale: 1.2, x: 100 }}
-const FlippingPages = () => {
+
+const FlippingBook = () => {
     const pages = Array(8).fill(0);
     return (
         <motion.div className="relative w-[400px] h-[500px] perspective-[800px]">
@@ -25,17 +24,8 @@ const FlippingPages = () => {
                     <div className="bg-white" />
                 </motion.div>
             ))}
-            <div
-                className="rounded-sm bg-yellow-400 z-0 origin-center h-full flex items-center justify-center"
-                style={{
-                    backgroundImage: `linear-gradient(45deg, rgba(0,0,0,0.1) 25%, transparent 25%, transparent 50%, rgba(0,0,0,0.1) 50%, rgba(0,0,0,0.1) 75%, transparent 75%, transparent)`,
-                    backgroundSize: '5px 5px',
-                    backgroundPosition: '0 0',
-                    backgroundRepeat: 'repeat',
-                }}
-            />
         </motion.div>
     );
 };
 
-export default FlippingPages;
+export default FlippingBook;
