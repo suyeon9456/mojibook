f0d0a4fd838c8532743010256d1d04340f1575fe
suyeon9456 <suyeon9456@naver.com>
2025-04-09 11:25:16 +0900

    refac: 플리핑 북 컴포넌트에 페이지 컴포넌트 분리

--- src/component/FlippingBook/index.tsx
+++ src/component/FlippingBook/index.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import BookCoverInner from './BookCoverInner';
+import BookPage from './BookPage';
 
 const FlippingBook = () => {
     const pages = Array(8).fill(0);
@@ -7,28 +8,11 @@ const FlippingBook = () => {
         <motion.div className="relative w-[400px] h-[500px] perspective-[800px]">
             <BookCoverInner className="absolute top-0 left-[-400px]" />
             {pages.map((_, index) => (
-                <motion.div
-                    className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden"
-                    key={index}
-                    style={{
-                        zIndex: pages.length - index,
-                        boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-                        transformOrigin: 'left',
-                    }}
-                    initial={{ rotateY: 0, skewY: 0 }}
-                    animate={{ rotateY: -180, skewY: 0 - (pages.length - index) }}
-                    transition={{
-                        duration: 0.6,
-                        delay: index * 0.1,
-                        ease: 'easeInOut',
-                    }}
-                >
-                    <div className="bg-white" />
-                </motion.div>
+                <BookPage key={index} index={index} totalPages={pages.length} />
             ))}
             <motion.div
-                className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden"
-                initial={{ rotateY: 0, skewY: -1 }}
+                className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden rounded-sm"
+                initial={{ rotateY: 0 }}
                 style={{
                     boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
                 }}
