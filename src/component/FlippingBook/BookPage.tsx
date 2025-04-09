0841b8e844de15dcaabd2146a1f86208d9570366
suyeon9456 <suyeon9456@naver.com>
2025-04-09 11:43:52 +0900

    style: 플리핑 책을 가운데 정렬되도록 수정

--- src/component/FlippingBook/BookPage.tsx
+++ src/component/FlippingBook/BookPage.tsx
@@ -7,30 +7,37 @@ interface BookPageProps {
 
 const BookPage = ({ index, totalPages }: BookPageProps) => {
     return (
-        <motion.div
-            className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden rounded-sm"
-            key={index}
-            style={{
-                zIndex: totalPages - index,
-                boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-                transformOrigin: 'left',
-            }}
-            initial={{ rotateY: 0 }}
-            animate={{ rotateY: -180 }}
-            transition={{
-                duration: 0.6,
-                delay: index * 0.1,
-                ease: 'easeInOut',
-            }}
-        >
-            <div className="bg-white" />
-            <div
-                className="absolute top-0 left-0 w-full h-full pointer-events-none"
-                style={{
-                    background: 'linear-gradient(to right, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0) 10%)',
-                }}
+        <>
+            <motion.div
+                className="absolute top-2 left-[404px] w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden rounded-sm"
+                initial={{ rotateY: 0 }}
             />
-        </motion.div>
+            <motion.div
+                className="absolute top-2 left-[404px] w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden rounded-sm"
+                key={index}
+                style={{
+                    zIndex: totalPages - index,
+                    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+                    transformOrigin: 'left',
+                }}
+                initial={{ rotateY: 0 }}
+                animate={{ rotateY: -180 }}
+                transition={{
+                    duration: 0.6,
+                    delay: index * 0.1,
+                    ease: 'easeInOut',
+                }}
+            >
+                <div className="bg-white" />
+                <div
+                    className="absolute top-0 left-0 w-full h-full pointer-events-none"
+                    style={{
+                        background:
+                            'linear-gradient(to right, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0) 10%)',
+                    }}
+                />
+            </motion.div>
+        </>
     );
 };
 
--- src/component/FlippingBook/index.tsx
+++ src/component/FlippingBook/index.tsx
@@ -5,18 +5,11 @@ import BookPage from './BookPage';
 const FlippingBook = () => {
     const pages = Array(8).fill(0);
     return (
-        <motion.div className="relative w-[400px] h-[500px] perspective-[800px]">
-            <BookCoverInner className="absolute top-0 left-[-400px]" />
+        <motion.div className="relative w-[800px] h-[500px] perspective-[800px]">
+            <BookCoverInner className="absolute top-0 left-0" />
             {pages.map((_, index) => (
                 <BookPage key={index} index={index} totalPages={pages.length} />
             ))}
-            <motion.div
-                className="absolute top-2 left-0 w-[380px] h-[480px] bg-white flex items-center justify-center backface-visibility-hidden rounded-sm"
-                initial={{ rotateY: 0 }}
-                style={{
-                    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
-                }}
-            />
             <BookCoverInner />
         </motion.div>
     );
