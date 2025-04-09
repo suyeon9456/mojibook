075c4ba504d40ede3a430878f979275e86e46eac
suyeon9456 <suyeon9456@naver.com>
2025-04-09 18:54:19 +0900

    style: ZoomPage 모바일 화면 스타일 적용

--- src/components/FlippingBook/page/ZoomPage.tsx
+++ src/components/FlippingBook/page/ZoomPage.tsx
@@ -1,21 +1,28 @@
+import classNames from 'classnames';
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 interface ZoomPageProps {
     message: string;
 }
 
 const ZoomPage = ({ message }: ZoomPageProps) => {
+    const isSmallScreen = useMediaQuery({ maxWidth: 768 });
     return (
         <motion.div
-            className="absolute top-0 left-0 w-full h-full bg-white box-shadow-lg p-40 z-100 flex items-center justify-center transform-origin-center pointer-events-auto rounded-sm"
+            className={classNames(
+                'absolute top-0 left-0 w-full h-full bg-white box-shadow-lg p-40 z-100 flex items-center justify-center transform-origin-center pointer-events-auto rounded-sm',
+            )}
             initial={{ scale: 1, rotateX: 20 }}
             animate={{ scale: 1.2, rotateX: 0, translateY: -20 }}
-            transition={{ duration: 0.8, ease: 'easeOut' }}
+            transition={{ duration: 0.5, ease: 'easeOut' }}
             style={{
                 boxShadow: '0 10px 40px rgba(0, 0, 0, 0.2)',
             }}
         >
-            <h2>{message}</h2>
+            <h2 className={classNames(isSmallScreen ? 'w-[100vw] text-lg px-[60px]' : 'text-lg')}>
+                {message}
+            </h2>
         </motion.div>
     );
 };
