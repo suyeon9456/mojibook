b174fc4f0a46fdce8ad6f0e18f8bcee77c93009a
suyeon9456 <suyeon9456@naver.com>
2025-04-11 17:21:55 +0900

    fix: IOS 디바이스에서는 센서 권한 요청을 한 뒤 이벤트핸들러를 설정할 수 있도록 센서 권한 요청 함수 분리 및 반환

--- src/hooks/useDeviceTilt.ts
+++ src/hooks/useDeviceTilt.ts
@@ -6,56 +6,56 @@ interface UseDeviceTiltOptions {
     maxTilt?: number;
 }
 
-// Define the type for the DeviceOrientationEvent constructor with the optional static method
 type DeviceOrientationEventConstructorWithPermission = typeof DeviceOrientationEvent & {
     requestPermission?: () => Promise<'granted' | 'denied'>;
 };
 
-const useDeviceTilt = ({ maxTilt = 40, isMobile, isIOS }: UseDeviceTiltOptions) => {
+const useDeviceTilt = ({ maxTilt = 80, isMobile, isIOS }: UseDeviceTiltOptions) => {
     const ref = useRef<HTMLDivElement | HTMLElement | null>(null);
+
+    const handleOrientation = (event: DeviceOrientationEvent) => {
+        const { beta, gamma } = event;
+
+        if (!ref.current || beta === null || gamma === null) return;
+
+        const rotateX = Math.max(Math.min((beta / 90) * maxTilt, maxTilt), -maxTilt);
+        const rotateY = Math.max(Math.min((gamma / 90) * maxTilt, maxTilt), -maxTilt);
+
+        ref.current.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    };
     useEffect(() => {
-        if (!isMobile) return;
-        const handleOrientation = (event: DeviceOrientationEvent) => {
-            const { beta, gamma } = event;
-
-            if (!ref.current || beta === null || gamma === null) return;
-
-            const rotateX = Math.max(Math.min((beta / 90) * maxTilt, maxTilt), -maxTilt);
-            const rotateY = Math.max(Math.min((gamma / 90) * maxTilt, maxTilt), -maxTilt);
-
-            ref.current.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        };
-
-        const requestPermission = async () => {
-            if (isIOS === true) {
-                const eventConstructor =
-                    DeviceOrientationEvent as DeviceOrientationEventConstructorWithPermission;
-                if (typeof eventConstructor.requestPermission === 'function') {
-                    try {
-                        const response = await eventConstructor.requestPermission();
-                        if (response === 'granted') {
-                            window.addEventListener('deviceorientation', handleOrientation, true);
-                        }
-                    } catch (err) {
-                        console.error('IOS Device orientation permission denied:', err);
-                    }
-                } else {
-                    window.addEventListener('deviceorientation', handleOrientation, true);
-                }
-            } else {
-                window.addEventListener('deviceorientation', handleOrientation, true);
-            }
-        };
-
-        requestPermission();
-
+        if (!isMobile || isIOS === true) return;
+        window.addEventListener('deviceorientation', handleOrientation, true);
         return () => {
             if (!isMobile) return;
             window.removeEventListener('deviceorientation', handleOrientation);
         };
-    }, [ref.current, maxTilt, isMobile, isIOS]);
+    }, [ref.current, maxTilt, isMobile, isIOS, handleOrientation]);
 
-    return { ref };
+    const requestPermission = async () => {
+        if (isIOS === false) {
+            window.addEventListener('deviceorientation', handleOrientation, true);
+            return;
+        }
+        const eventConstructor =
+            DeviceOrientationEvent as DeviceOrientationEventConstructorWithPermission;
+        if (typeof eventConstructor.requestPermission === 'function') {
+            try {
+                const response = await eventConstructor.requestPermission();
+                if (response === 'granted') {
+                    window.addEventListener('deviceorientation', handleOrientation, true);
+                } else {
+                    console.warn('Permission denied.');
+                }
+            } catch (err) {
+                console.error('IOS Device orientation permission denied:', err);
+            }
+        } else {
+            window.addEventListener('deviceorientation', handleOrientation, true);
+        }
+    };
+
+    return { ref, requestPermission };
 };
 
 export default useDeviceTilt;
