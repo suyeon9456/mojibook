dbe4b613b89f3abdd581deec9a1e0798c62c9bd1
suyeon9456 <suyeon9456@naver.com>
2025-04-15 21:36:18 +0900

    style: mobile 화면에서 스크롤 생기는 이슈 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
+import VhSetter from '@/components/common/VhSetter';
 import MojiBookPage from '@/containers/MojiBookPage';
 import { cookies } from 'next/headers';
 
 export default async function Home() {
     const deviceType = await getDeviceType();
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold text-center text-white">
-                지금 마음속에 있는 걸 떠올리며
-                <br />
-                클릭해보세요.
-            </h1>
-            <MojiBookPage isMobile={deviceType === 'mobile'} />
-        </div>
+        <>
+            <VhSetter />
+            <div
+                className="flex flex-col items-center justify-center"
+                style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+            >
+                <h1 className="text-2xl font-bold text-center text-white">
+                    지금 마음속에 있는 걸 떠올리며
+                    <br />
+                    클릭해보세요.
+                </h1>
+                <MojiBookPage isMobile={deviceType === 'mobile'} />
+            </div>
+        </>
     );
 }
 
