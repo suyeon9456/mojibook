7531aaa857e09dca0f8f3495ccf17707e9d57d36
suyeon9456 <suyeon9456@naver.com>
2025-04-18 17:10:05 +0900

    feat: 메시지 생성 시 DB 저장 및 쿼리 파라미터로 메시지 조회 기능 추가

--- src/containers/MojiBookPage/index.tsx
+++ src/containers/MojiBookPage/index.tsx
@@ -1,45 +1,46 @@
 'use client';
 
 import { useCallback, useMemo, useState } from 'react';
-import useMojiMessage from '@/app/hooks/api/useMojiMessage';
 import InteractiveBook from '@/components/InteractiveBook';
 import FlippingBook from '@/components/FlippingBook';
 import useDeviceTilt from '@/hooks/useDeviceTilt';
 import Button from '@/components/common/Button';
 import { useRouter, useSearchParams } from 'next/navigation';
+import useMojiMessage from '@/hooks/api/useMojiMessage';
+import useMessage from '@/hooks/api/useMessage';
 
 const MojiBook = ({ isMobile, isIOS }: { isMobile: boolean; isIOS: boolean }) => {
     const router = useRouter();
     const searchParams = useSearchParams();
+    const messageId = useMemo(() => searchParams.get('id'), []);
+    const { insertMessage, message: prevMessage } = useMessage({
+        messageId: messageId == null ? undefined : Number(messageId),
+    });
     const {
         data: message,
         isTransitioning,
         mutateWithTransition: getMojiMessage,
     } = useMojiMessage({
-        onSuccess: (data) => {
+        onSuccess: async (message) => {
+            const { id } = await insertMessage(message);
             const params = new URLSearchParams(searchParams.toString());
-            params.set('message', btoa(encodeURIComponent(data.toString())));
+            params.set('id', String(id));
             router.push(`?${params.toString()}`);
         },
     });
-    const [bookOpen, setBookOpen] = useState(searchParams.get('message') != null);
+    const [bookOpen, setBookOpen] = useState(messageId != null);
     const { ref: bookRef, requestPermission } = useDeviceTilt({ maxTilt: 40, isMobile, isIOS });
 
     const handleBookClick = useCallback(() => {
         setBookOpen(true);
     }, []);
 
-    const prevMessage = useMemo(
-        () => decodeURIComponent(atob(searchParams.get('message') ?? '')),
-        [searchParams.get('message')],
-    );
-
     return (
         <div>
             {bookOpen ? (
                 <FlippingBook
                     getMojiMessage={getMojiMessage}
-                    message={prevMessage || message || ''}
+                    message={message || prevMessage?.message || ''}
                     isLoading={isTransitioning}
                 />
             ) : (
